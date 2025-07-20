420472a0d9b936489886077b7a820a0d7c99e9cb
MIHAN Zlanca-N'to <mihanzlancanto@gmail.com>
2025-07-20 11:33:11 +0200

    Routes API modifiées

--- src/vars/api.ts
+++ src/vars/api.ts
@@ -5,30 +5,30 @@ const post = 'api/post';
 export const API_URL = process.env.REACT_APP_API_URL;
 
 /* JWT */
-export const API_JWT = `${API_URL}jwtId`;
+export const API_JWT = `${API_URL}/jwtId`;
 
 /* User */
-export const API_USER_LOGIN = `${API_URL}${user}/login`;
-export const API_USER_LOGOUT = `${API_URL}${user}/logout`;
-export const API_USER_REGISTER = `${API_URL}${user}/register`;
-export const API_USER_ALL = `${API_URL}${user}/`;
-export const API_USER_BY_ID = `${API_URL}${user}`;
-export const API_USER_FOLLOW = `${API_URL}${user}/follow`;
-export const API_USER_UNFOLLOW = `${API_URL}${user}/unfollow`;
-export const API_USER_UPLOAD_PICTURE = `${API_URL}${user}/upload`;
+export const API_USER_LOGIN = `${API_URL}/${user}/login`;
+export const API_USER_LOGOUT = `${API_URL}/${user}/logout`;
+export const API_USER_REGISTER = `${API_URL}/${user}/register`;
+export const API_USER_ALL = `${API_URL}/${user}/`;
+export const API_USER_BY_ID = `${API_URL}/${user}`;
+export const API_USER_FOLLOW = `${API_URL}/${user}/follow`;
+export const API_USER_UNFOLLOW = `${API_URL}/${user}/unfollow`;
+export const API_USER_UPLOAD_PICTURE = `${API_URL}/${user}/upload`;
 
 /* Post */
-export const API_POST_ALL = `${API_URL}${post}/`;
-export const API_POST_CREATE = `${API_URL}${post}/`;
-export const API_POST_UPDATE = `${API_URL}${post}`;
-export const API_POST_DELETE = `${API_URL}${post}`;
-export const API_POST_LIKE = `${API_URL}${post}/likePost`;
-export const API_POST_UNLIKE = `${API_URL}${post}/unlikePost`;
+export const API_POST_ALL = `${API_URL}/${post}/`;
+export const API_POST_CREATE = `${API_URL}/${post}/`;
+export const API_POST_UPDATE = `${API_URL}/${post}`;
+export const API_POST_DELETE = `${API_URL}/${post}`;
+export const API_POST_LIKE = `${API_URL}/${post}/likePost`;
+export const API_POST_UNLIKE = `${API_URL}/${post}/unlikePost`;
 
 /* Comment */
-export const API_COMMENT_CREATE = `${API_URL}${post}/commentPost`;
-export const API_COMMENT_EDIT = `${API_URL}${post}/editCommentPost`;
-export const API_COMMENT_DELETE = `${API_URL}${post}/deleteCommentPost`;
+export const API_COMMENT_CREATE = `${API_URL}/${post}/commentPost`;
+export const API_COMMENT_EDIT = `${API_URL}/${post}/editCommentPost`;
+export const API_COMMENT_DELETE = `${API_URL}/${post}/deleteCommentPost`;
 
 
 
